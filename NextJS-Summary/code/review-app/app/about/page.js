17d1d6d8308ed29a5aac7745f8676c1ef73117db
dnjsxoghd <feelchok1234@gmail.com>
2024-12-25 20:05:49 +0900

    reviewing...

---
+++ NextJS-Summary/code/review-app/app/about/page.js
@@ -0,0 +1,12 @@
+function About(){
+
+    console.log('about')
+
+    return(
+        <>
+            <h1>About Page</h1>
+        </>
+    )
+}
+
+export default About;
---
+++ NextJS-Summary/code/review-app/app/blog/[blog]/page.js
@@ -0,0 +1,11 @@
+function PBlog({params}){
+
+    return(
+        <>
+            <h3>Hello-PBlog</h3>
+            <div>{params.blog}</div>
+        </>
+    )
+}
+
+export default PBlog;
