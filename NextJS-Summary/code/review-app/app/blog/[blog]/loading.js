8a08e85a0a7a532edfc4bb6b0fbce5d65c14b014
dnjsxoghd <feelchok1234@gmail.com>
2025-05-14 18:16:40 +0900

    initialize

--- NextJS-Summary/code/review-app/app/blog/[blog]/loading.js
+++
@@ -1,9 +0,0 @@
-function SeparateBlogLoading() {
-  return (
-    <>
-      <p className="text-center text-4xl text-red-600">Loading....</p>
-    </>
-  );
-}
-
-export default SeparateBlogLoading;
--- Pages-Router-Code/14-NextJS-PR-Project/pages/contact.js
+++
@@ -1,7 +0,0 @@
-import ContactForm from "@/components/contact/ContactForm";
-
-function ContactPage() {
-  return <ContactForm></ContactForm>;
-}
-
-export default ContactPage;
